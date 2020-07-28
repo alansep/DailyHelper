31578c776868e95868c24fd96752ec51d944a015
Gabriel Alan <alang.von@gmail.com>
2020-07-27 23:04:02 -0300

    Build options configured.

--- src/main/java/br/com/alansep/dailyhelper/Runner.java
+++ src/main/java/br/com/alansep/dailyhelper/Runner.java
@@ -1,9 +1,12 @@
 package br.com.alansep.dailyhelper;
 
+import br.com.alansep.dailyhelper.view.Home;
+
 public class Runner {
 
 	public static void main(String[] args) {
-		
+		Home home = new Home();
+		home.setVisible(true);
 	}
 	
 }
